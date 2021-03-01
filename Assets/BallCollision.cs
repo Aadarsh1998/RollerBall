0624fb42c5cbb205a8d1d94ee369cb00b13dcffd
DESKTOP-Aadarsh <aadarshkomera@gmail.com>
2021-03-01 19:58:31 +0530

    GitIgnore Commited

---
+++ Assets/BallCollision.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallCollision : MonoBehaviour
+{
+    public Material[] wallMat;
+    Renderer rend;
+    public Text displayText;
+    public Text displayText2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        displayText.text = "";
+        displayText2.text = "";
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "ball") 
+        {
+            displayText.text = "Player 1 hit the wall";
+            rend.sharedMaterial = wallMat[1];
+        }
+        if (collision.gameObject.name == "ball2")
+        {
+            displayText2.text = "Player 2 hit the wall";
+            rend.sharedMaterial = wallMat[1];
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.name=="ball")
+        {
+            rend.sharedMaterial = wallMat[0];
+            displayText.text = "Player 1 keep rolling";
+        }
+        if (collision.gameObject.name == "ball2")
+        {
+            rend.sharedMaterial = wallMat[0];
+            displayText2.text = "Player 2 keep rolling";
+        }
+    }
+}
---
+++ Assets/BallRoller2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRoller2 : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("newHorizontal");
+        float verticalMovement = Input.GetAxis("newVertical");
+        Vector3 movement = new Vector3(horizontalMovement, 0, verticalMovement);
+        rb2.AddForce(movement * speed * Time.deltaTime);
+    }
+}
